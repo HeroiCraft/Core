7117bdb894410e37ba28c087c1aab7c95db0b101
HeroCC <HeroCC@herocc.com>
2016-02-06 11:32:40 -0500

    Correct fly speed

--- src/main/java/com/herocc/bukkit/core/api/SpeedAPI.java
+++ src/main/java/com/herocc/bukkit/core/api/SpeedAPI.java
@@ -4,7 +4,7 @@ import org.bukkit.entity.Player;
 
 public class SpeedAPI {
   public void resetSpeed(Player player){
-    player.setFlySpeed(0.2F);
+    player.setFlySpeed(0.1F);
     player.setWalkSpeed(0.2F);
     }
 }
